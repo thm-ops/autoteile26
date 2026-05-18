444acaf2a98dfcc078fadccf3cc77bf8d0eb1bda
Noel Hoppe <noel.hoppe@gmx.de>
2026-05-18 18:49:15 +0200

    documentation

--- packages/apps/web/src/domain/product/index.ts
+++ packages/apps/web/src/domain/product/index.ts
@@ -1,1 +1,5 @@
+/**
+ * Package containing product related types like
+ * product request data transfer objects, product response data transfer objects, and product entity models.
+ */
 export * from "./product.view";
