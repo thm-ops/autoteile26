f2aa05059987d1c79d37c46ed4a2cb4a0458d972
Noel Hoppe <noel.hoppe@gmx.de>
2026-05-29 17:43:51 +0200

    fix(DI): module resolution error

--- packages/apps/service/src/health/health.controller.spec.ts
+++ packages/apps/service/src/health/health.controller.spec.ts
@@ -1,12 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { HealthController } from './health.controller';
+import { HealthModule } from './health.module';
 
 describe('HealthController', () => {
   let controller: HealthController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [HealthController],
+      imports: [HealthModule],
     }).compile();
 
     controller = module.get<HealthController>(HealthController);
