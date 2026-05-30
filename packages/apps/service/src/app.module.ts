c236652c270ca5ea06b67d845e091c3d1dfff77f
Noel Hoppe <noel.hoppe@gmx.de>
2026-05-30 21:55:24 +0200

    feat(Configuration, Database): NestJS Configuration and Database, see: https://docs.nestjs.com/techniques/configuration https://docs.nestjs.com/techniques/database#typeorm-integration

--- packages/apps/service/src/app.module.ts
+++ packages/apps/service/src/app.module.ts
@@ -2,9 +2,28 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HealthModule } from './health/health.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [HealthModule],
+  imports: [
+    HealthModule,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        username: configService.get('POSTGRES_USER')!,
+        password: configService.get('POSTGRES_PASSWORD')!,
+        database: configService.get('POSTGRES_DB')!,
+        port: configService.get('POSTGRES_PORT')!,
+        host: configService.get('POSTGRES_HOST')!,
+        synchronize: configService.get('NODE_ENV')! === 'development',
+        logging: true,
+      }),
+    }),
+    ConfigModule.forRoot(),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
