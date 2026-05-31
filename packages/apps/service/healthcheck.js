8f952faa59be8be5053613011bec058bb8da5ff0
Noel Hoppe <noel.hoppe@gmx.de>
2026-05-31 17:00:10 +0200

    feat(dockerize prod env): dockerize postgres for prod, service depends on healthy db, web app gets api base url but does not depend on service healthy due to health check issues

---
+++ packages/apps/service/healthcheck.js
@@ -0,0 +1,35 @@
+// https://www.mattknight.io/blog/docker-healthchecks-in-distroless-node-js
+const http = require('node:http');
+
+const options = { hostname: 'localhost', port: 3000, path: '/api/health', method: 'GET' };
+
+console.log(`Performing health check for ${options.method} ${options.hostname}:${options.port}${options.path}`)
+
+http
+    .request(options, (res) => {
+        let body = '';
+
+        res.on('data', (chunk) => {
+            body += chunk;
+        });
+
+        res.on('end', () => {
+            try {
+                const response = JSON.parse(body);
+                if (response.healthy === true) {
+                    process.exit(0);
+                }
+
+                console.log('Unhealthy response received: ', body);
+                process.exit(1);
+            } catch (err) {
+                console.log('Error parsing JSON response body: ', err);
+                process.exit(1);
+            }
+        });
+    })
+    .on('error', (err) => {
+        console.log('Error: ', err);
+        process.exit(1);
+    })
+    .end();
