5a88388221ed82a75bda987d8125633ecaf3fa99
Christian Heinz <christian.heinz-2@mni.thm.de>
2026-06-11 10:29:17 +0200

    feat: adds product page showing product details.

---
+++ packages/apps/web/app/components/product/ProductDetails.tsx
@@ -0,0 +1,54 @@
+import Image from "next/image"
+import Link from "next/link"
+import { formatPrice } from "@/app/utils/formatPrice"
+import { ProductView} from "@/app/domain/product";
+
+type ProductDetailsProps = {
+    product : ProductView;
+};
+
+export default function ProductDetails({ product }: ProductDetailsProps) {
+    const formattedPrice = formatPrice(product.price)
+
+    return (
+        <main className="max-w-5xl mx-auto px-4 py-10">
+            <Link href="/" className="text-blue-600 hover:underline">
+                Zurück zur Übersicht
+            </Link>
+
+            <section className="grid grid-cols-1 md:grid-cols-2 gap-10 mt-6">
+                <div className="rounded overflow-hidden shadow-lg bg-card">
+                    <Image
+                        src={product.image || "https://placehold.co/600x400"}
+                        width={600}
+                        height={400}
+                        alt={product.imageAlt || product.name}
+                        unoptimized={true}
+                        className="w-full h-auto"
+                    />
+                </div>
+
+                <div>
+                    <h1 className="font-bold text-3xl mb-4">
+                        {product.name}
+                    </h1>
+
+                    <p className="text-foreground text-lg mb-6">
+                        {product.description}
+                    </p>
+
+                    <p className="text-2xl font-semibold mb-8">
+                        {formattedPrice}
+                    </p>
+
+                    <button
+                        type="button"
+                        className="bg-blue-600 text-white p-3 rounded-2xl inline-block"
+                    >
+                        Sofort kaufen
+                    </button>
+                </div>
+            </section>
+        </main>
+    );
+}
