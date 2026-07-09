c06d23606b1df9fdce12881fdd8307b7696fda79
Noel Hoppe <noel.hoppe@gmx.de>
2026-07-09 19:47:17 +0200

    feat: validate incoming requests based on shared zod schemas server-side

--- packages/apps/service/src/auth/auth.controller.ts
+++ packages/apps/service/src/auth/auth.controller.ts
@@ -1,59 +1,54 @@
 import {
-    Controller,
-    Post,
-    Body,
-    UnauthorizedException,
+  Controller,
+  Post,
+  Body,
+  UnauthorizedException,
+  UsePipes,
 } from '@nestjs/common';
 import { UserService } from '../user/user.service';
 import { JwtService } from '@nestjs/jwt';
+import { loginDto, registerDto } from '@autoteile26/shared';
+import type { LoginDto, RegisterDto } from '@autoteile26/shared';
+import { ZodValidationPipe } from '../validation/ZodValidationPipe';
 
 @Controller('auth')
 export class AuthController {
-    constructor(
-        private readonly userService: UserService,
-        private readonly jwtService: JwtService,
-    ) { }
+  constructor(
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService,
+  ) {}
 
-    @Post('login')
-    async login(
-        @Body() body: {
-            email: string;
-            password: string;
-        },
-    ) {
-        const user = await this.userService.validateUser(body.email, body.password,);
+  @Post('login')
+  @UsePipes(new ZodValidationPipe(loginDto))
+  async login(@Body() body: LoginDto) {
+    const user = await this.userService.validateUser(body.email, body.password);
 
-        if (!user) {
-            throw new UnauthorizedException('Invalid credentials');
-        }
-
-        const payload = {
-            sub: user.id,
-            email: user.email,
-        };
-
-        return {
-            access_token: this.jwtService.sign(payload),
-            user: {
-                id: user.id,
-                email: user.email,
-            },
-        };
+    if (!user) {
+      throw new UnauthorizedException('Invalid credentials');
     }
 
-    @Post('register')
-    async register(
-        @Body()
-        body: {
-            email: string;
-            password: string;
-        },
-    ) {
-        const user = await this.userService.createUser(body.email, body.password,);
+    const payload = {
+      sub: user.id,
+      email: user.email,
+    };
 
-        return {
-            id: user.id,
-            email: user.email,
-        };
-    }
+    return {
+      access_token: this.jwtService.sign(payload),
+      user: {
+        id: user.id,
+        email: user.email,
+      },
+    };
+  }
+
+  @Post('register')
+  @UsePipes(new ZodValidationPipe(registerDto))
+  async register(@Body() body: RegisterDto) {
+    const user = await this.userService.createUser(body.email, body.password);
+
+    return {
+      id: user.id,
+      email: user.email,
+    };
+  }
 }
