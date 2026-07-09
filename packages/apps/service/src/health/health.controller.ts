45f588824ac1d0b44ff45540f4409d57892dd16d
Noel Hoppe <noel.hoppe@gmx.de>
2026-07-09 22:28:47 +0200

    fix: remove healthcheck targeting backend itself
     Reasoning: This healthcheck gets already covered by the healthcheck endpoint itself
     We only have to check for backend's external dependencies such as databases, external apis

--- packages/apps/service/src/health/health.controller.ts
+++ packages/apps/service/src/health/health.controller.ts
@@ -2,29 +2,19 @@ import { Controller, Get } from '@nestjs/common';
 import {
   HealthCheck,
   HealthCheckService,
-  HttpHealthIndicator,
   TypeOrmHealthIndicator,
 } from '@nestjs/terminus';
-import { ConfigService } from '@nestjs/config';
 
 @Controller('/api/health')
 export class HealthController {
   constructor(
-    private health: HealthCheckService,
-    private http: HttpHealthIndicator,
-    private typeOrm: TypeOrmHealthIndicator,
-    private configService: ConfigService,
+    private readonly health: HealthCheckService,
+    private readonly typeOrm: TypeOrmHealthIndicator,
   ) {}
 
   @Get()
   @HealthCheck()
   check() {
-    const servicePort =
-      this.configService.get<string>('SERVICE_PORT') ?? '3001';
-
-    return this.health.check([
-      () => this.http.pingCheck('backend', `http://localhost:${servicePort}`),
-      () => this.typeOrm.pingCheck('database'),
-    ]);
+    return this.health.check([() => this.typeOrm.pingCheck('database')]);
   }
 }
