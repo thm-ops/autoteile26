e23ea87edfec9c4c0bd24d17779d14a6e6b5d013
Noel Hoppe <noel.hoppe@gmx.de>
2026-07-10 08:25:52 +0200

    refactor(password schema): must contain at least one special character

--- packages/libs/shared/src/schemas/auth/passwordSchema.ts
+++ packages/libs/shared/src/schemas/auth/passwordSchema.ts
@@ -4,5 +4,6 @@ const passwordSchema = z
     .string()
     .min(12, "Password must be at least 12 characters")
     .regex(/[a-zA-Z]/, "Must contain at least one letter")
-    .regex(/\d/, "Must contain at least one number");
+    .regex(/\d/, "Must contain at least one number")
+    .regex(/[^a-zA-Z0-9]/g, "Must contain at least one special character");
 export default passwordSchema;
