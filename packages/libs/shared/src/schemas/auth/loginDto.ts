a2d9c1e7040942e63e968c5d11042a6163dc0179
Noel Hoppe <noel.hoppe@gmx.de>
2026-07-10 08:05:16 +0200

    fix: suggestions from @blhn25

--- packages/libs/shared/src/schemas/auth/loginDto.ts
+++ packages/libs/shared/src/schemas/auth/loginDto.ts
@@ -1,9 +1,8 @@
 import * as z from "zod"
-import {registerDto} from "./registerDto.js";
 
 export const loginDto = z.object({
     email: z.email(),
     password: z.string()
 })
 
-export type LoginDto = z.infer<typeof registerDto>
+export type LoginDto = z.infer<typeof loginDto>
